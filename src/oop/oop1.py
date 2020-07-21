74c5ac976416e2874b68e382d1207ba3b4639179
Francis2023 <56661250+Francis2023@users.noreply.github.com>
2020-07-20 18:51:15 -0600

    2nd update

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,19 +17,59 @@
 #     pass
 
 # Put a comment noting which class is the base class
+# Vechile is the base class
 class Vehicle:
-    def __init__(self,name,groundVehicle=[],flightVehicle=()):
+    def __init__(self,name,groundVehicle=[],flightVehicle=[]):
         self.name = name
         self.groundVehicle = groundVehicle
         self.flightVehicle = flightVehicle
 
 class GroundVehicle:
-    def __init__(self,name):
-        self.name
+    def __init__(self,type):
+        self.type
+    
+    def __str__(self):
+        return (f"{self.type}")
 
 class Car(GroundVehicle):
-    def __init__(self, name, mileage):
+    def __init__(self, name, mileage, type):
         super().__init__(name)
         self.name
+        self.mileage
 
+    def __str__(self):
+        return (f"{self.name}, {self.mileage}, {self.type}")
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, mileage, type):
+        super().__init__(name)
+        self.name
+        self.mileage
+
+    def __str__(self):
+        return (f"{self.name}, {self.mileage}, {self.type}")
+
+class FlightVehicle:
+    def __init__(self,type):
+        self.type
+    
+    def __str__(self):
+        return (f"{self.type}")
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.name
+       
+
+    def __str__(self):
+        return (f"{self.name}, {self.type}")
+
+class StarShip(FlightVehicle):
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.name
         
+
+    def __str__(self):
+        return (f"{self.name}, {self.type}")
